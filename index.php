8d0a524de17e0fa9a0a2dfbec8ef4be5d5c1be6e
joshuaRMS <jcdslatu@avans.nl>
2017-04-13 15:00:28 +0200

    Init commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joshua
+ * Date: 13-4-2017
+ * Time: 14:58
+ */
+
+?>
+
+<html>
+<head>
+    <title>Portfolio Joshua Latusia</title>
+</head>
+<body>
+<p>Work In Progress</p>
+</body>
+</html>
